3edc828baedc6c45bdaa97b6c15a3ec725e90f95
ken_ruster <ken.ruster@gmail.com>
2024-04-15 19:47:50 +0800

    Debug

--- convolve.s
+++ convolve.s
@@ -5,11 +5,11 @@
  *      Author: Hou Linxin
  */
    .syntax unified
-	.cpu cortex-m4
-	.fpu softvfp
-	.thumb
+    .cpu cortex-m4
+    .fpu softvfp
+    .thumb
 
-		.global convolve
+        .global convolve
 
 @ Start of executable code
 .section .text
@@ -22,107 +22,118 @@
 
 @ You could create a look-up table of registers here:
 
-@ R0 Address of h
+@ R0 Address of h --> Address of Y
 @ R1 Address of x
-@ R2 hlen
-@ R3 xlen
+@ R2 lenH
+@ R3 lenX
 
 @ R4 nConv = length of output array y
 @ R5 y[i]
 @ R6 index if loop 1 i
-@ R7 x_start
+@ R7 x_start --> index j
 @ R8 lenX --> x_end
 @ R9 lenH --> h_start
-@ R10 index of 2nd loop j
+@ R10 Address of X
 @ R11 Address of h
-@ R13 Address of X
+@ R12 index j
 
 @ write your program from here:
 convolve:
-	PUSH {R14}
+    PUSH {R4, R5, R6, R7, R8, R9, R10, R11, R14}
 
-	@ Calculate and store nconv
-	ADD R4, R2, R3
-	SUB R4, #1
+    @ Calculate and store nconv
+    ADD R4, R2, R3
+    SUB R4, #1
 
-	@ move values to be used in loops
-	MOV R8, R3
-	MOV R9, R2
-	MOV R11, R0
-	MOV R13, R1
+    @ move values to be used in loops
+    MOV R8, R3
+    MOV R9, R2
+    MOV R11, R0
+    MOV R10, R1
 
-	@ initialise loop1
-	MOV R6, #0
+    @ initialise loop1
+    MOV R6, #0
 
-	@ Convolve
-	BL LOOP1
+    LDR R0, =Y
 
-	@ Pop address to main
-	POP {R14}
+    @ Convolve
+    BL LOOP1
 
-	@ output Y
-	LDR R0, =Y
-	BX LR
+    LDR R0, =Y
+
+    @ Pop address to main
+    POP {R4, R5, R6, R7, R8, R9, R10, R11, R14}
+
+    BX LR
 
 LOOP1:
-	PUSH {R14}
+    PUSH {R4, R6, R8, R9, R14}
 
-	@ temporarily store lenH in R2
-	MOV R2, R9
+    @ temporarily store lenH in R2
+    MOV R2, R9
 
-	@ calc h_start
-	SUB R9, #1
-	CMP R9, R8
-	IT LT
-	MOV R9, R8
+    @ x_start = MAX(0,i-lenH+1);
+    @ x_end   = MIN(i+1,lenX);
+    @ h_start = MIN(i,lenH-1);
 
-	@ calc x_end
-	@ temporarily store i + 1 in R3
-	ADD R3, R6, #1
-	CMP R8, R3
-	IT LT
-	MOV R8, R3
+    @ calc h_start
+    SUB R9, #1
+    CMP R9, R6
+    IT HS
+    MOVHS R9, R6
 
-	@ calc x_start
-	SUBS R7, R2, R12
-	IT MI
-	MOV R7, #0
+    @ calc x_end
+    @ temporarily store i + 1 in R3
+    ADD R3, R6, #1
+    CMP R8, R3
+    IT HS
+    MOVHS R8, R3
 
-	@ initialize 2nd loop
-	MOV R7, R10
-	MOV R5, #0
+    @ calc x_start
+    SUBS R7, R3, R2
+    IT MI
+    MOVMI R7, #0
 
-	BL LOOP2
+    @ initialize 2nd loop
+    MOV R12, R7
+    MOV R5, #0
 
-	@ store value of y[i] from R5 that we got from loop2
-	STR R5, [Y, R6]
+    BL LOOP2
 
-	@ Loop logic
-	ADD R6, #1
-	CMP R6, R4
-	BLT LOOP1
+    @ store value of y[i] from R5 that we got from loop2
+    STR R5, [R0], #4
 
-	POP {R14}
-	BX LR
+    @ restore values
+    POP {R4, R6, R8, R9, R14}
+
+    @ Loop logic
+    ADD R6, #1
+    CMP R6, R4
+    BLT LOOP1
+
+    BX LR
 
 LOOP2:
-	@ R0 = h[h_start--]
-	LDR R0, [R11, R9]
-	SUB R9, #1
+    @ R8 = h[h_start--]
+    LSL R9, R9, #2
+    LDR R4, [R11, R9]
+    LSR R9, R9, #2
+    SUB R9, #1
 
-	@ R1 = x[j]
-	LDR R1, [R10]
+    @ R6 = x[j]
+    LSL R12, R12, #2
+    LDR R6, [R10, R12]
+    LSR R12, R12, #2
 
-	@ Update y[i]
-	MLA R5, R0, R1, R5
+    @ Update y[i]
+    MLA R5, R6, R4, R5
 
-	@ loop logic
-	ADD R10, #1
+    @ loop logic
+    ADD R12, #1
+    CMP R12, R8
+    BLT LOOP2
 
-	CMP R10, R8
-	BLT LOOP2
-
-	BX LR
+    BX LR
 
 
 @ initialize output array, max size 20 words
